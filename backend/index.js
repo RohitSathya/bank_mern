0d54b10506a0abd8c207ea252531be6a3970238f
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-05-21 20:38:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,35 +6,14 @@ const Authroute=require('./router/AuthRoute')
 const AdminRoute=require('./router/AdminRoute')
 const TransactionRoute=require('./router/TransactionRoute')
 const chatRoute=require('./router/ChatRoute')
-const socketio = require('socket.io');
 app.use(express.json())
 app.use(cors())
 const nodemailer=require('nodemailer')
-const http=require('http')
 app.use('/api/auth',Authroute)
 app.use('/api/admin',AdminRoute)
 app.use('/api/transaction',TransactionRoute)
 app.use('/api/chat',chatRoute)
-const server=http.createServer(app)
-const io=socketio(server,{
-  cors:{
-     origin:"http://localhost:3000",
-     credentials:true
-    
-    
-  },
-})
 
-const corsOptions = {
-  origin: 'https://bank-front-eight.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-// Handle preflight requests
-app.options('*', cors(corsOptions));
 
 const transport=nodemailer.createTransport({
   service:"gmail",
@@ -43,21 +22,12 @@ const transport=nodemailer.createTransport({
       pass:"xtdekcpwnjsspsip"
   }
 })
-io.on('connection',(socke)=>{
-  console.log('hi')
-  socke.on('msg',(data)=>{
-    console.log('fjdfkdjfdkjfdkjf')
-    console.log(data)
-      // const {to,message}=data
-      // console.log(to,message)
-      // io.emit(to,message)
-  })
-})
+
 
 
 
 mongoose.connect('mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/Bank?retryWrites=true&w=majority&appName=RoyoApi').then(()=>{
 
      console.log('db connected')
-     app.listen(5000,()=>console.log('server running'))
+     app.listen(8080,()=>console.log('server running'))
 })
