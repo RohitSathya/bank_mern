946b0cca26c404d40ddb112539019ea756ab0a4e
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-05-21 20:43:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const TransactionRoute=require('./router/TransactionRoute')
 const chatRoute=require('./router/ChatRoute')
 app.use(express.json())
 app.use(cors())
-const nodemailer=require('nodemailer')
 app.use('/api/auth',Authroute)
 app.use('/api/admin',AdminRoute)
 app.use('/api/transaction',TransactionRoute)
@@ -19,19 +18,11 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
-const transport=nodemailer.createTransport({
-  service:"gmail",
-  auth:{
-      user:"spiritroyo2336@gmail.com",
-      pass:"xtdekcpwnjsspsip"
-  }
-})
-
 
 
 
 mongoose.connect('mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/Bank?retryWrites=true&w=majority&appName=RoyoApi').then(()=>{
 
      console.log('db connected')
-     app.listen(8080,()=>console.log('server running'))
+     app.listen(8081,()=>console.log('server running'))
 })
